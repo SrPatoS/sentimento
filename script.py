4637afc296cec358f53f812f9a87ea924ea21d96
unknown <vavizvini000@gmail.com>
2025-04-03 20:47:59 -0300

    adiciona tradução

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import nltk
+from googletrans import Translator
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
 
 analyzer = SentimentIntensityAnalyzer()
+
+def traduzir_texto(texto, src_lang='pt', dest_lang='en'):
+    translator = Translator()
+    traducao = translator.translate(texto, src=src_lang, dest=dest_lang)
+    return traducao.text
 
 def analisar_sentimento(texto):
     pontuacao = analyzer.polarity_scores(texto)
@@ -18,6 +24,7 @@ texto = "I'm irritated"
 
 while True:
     texto = input("Digite algo: ")
+    texto = traduzir_texto(texto)
     sentimento = analisar_sentimento(texto)
     print(f"O sentimento do texto é: {sentimento}")
 
